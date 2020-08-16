8d75e1690850addcebfbbf2c84dba0057a595f54
Al-Mamun <mamun@leapinglogic.com>
2020-08-16 15:06:59 +0600

    Added a new display

--- hello.java
+++ hello.java
@@ -1,5 +1,17 @@
 public class hello {
     public static void main(string[] args) {
         System.out.println("Vs code is Awdsome");
+
+        greeting ("HM Nayem");
     }
+
+
+
+public static void gretting(string name){
+    System.out.println("Welcome " + name);
 }
+
+
+}
+
+
