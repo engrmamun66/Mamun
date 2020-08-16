dcc352f7f97b4eb69504ac9f396dd24c225d08c7
Al-Mamun <mamun@leapinglogic.com>
2020-08-16 15:26:30 +0600

    Change greating method

--- hello.java
+++ hello.java
@@ -2,13 +2,13 @@ public class hello {
     public static void main(string[] args) {
         System.out.println("Vs code is Awdsome");
 
-        greeting ("HM Nayem");
+        greeting ("Git");
     }
 
 
 
 public static void gretting(string name){
-    System.out.println("Welcome " + name);
+    System.out.println("Bye bye " + name);
 }
 
 
